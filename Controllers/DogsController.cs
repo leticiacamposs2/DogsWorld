160aebc75b28772c4e6d6c5696671956d02af397
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2025-07-21 01:01:40 -0300

    feat: update lista de dogs

--- Controllers/DogsController.cs
+++ Controllers/DogsController.cs
@@ -12,12 +12,28 @@ namespace DogsWorld.Controllers
     [ApiController]
     public class DogsController : ControllerBase
     {
-        // Mock data (in-memory list)
         private static List<Dog> _dogs = new List<Dog>
         {
             new Dog { Id = 1, Name = "Rex", Breed = "Labrador", Age = 5, BirthDate = DateTime.Now.AddYears(-5), HasOwner = true },
             new Dog { Id = 2, Name = "Max", Breed = "Poodle", Age = 3, BirthDate = DateTime.Now.AddYears(-3), HasOwner = false },
-            new Dog { Id = 3, Name = "Bella", Breed = "Bulldog", Age = 2, BirthDate = DateTime.Now.AddYears(-2), HasOwner = true }
+            new Dog { Id = 3, Name = "Bella", Breed = "Bulldog", Age = 2, BirthDate = DateTime.Now.AddYears(-2), HasOwner = true },
+            new Dog { Id = 4, Name = "Thor", Breed = "Pastor Alemão", Age = 4, BirthDate = DateTime.Now.AddYears(-4), HasOwner = true },
+            new Dog { Id = 5, Name = "Luna", Breed = "Golden Retriever", Age = 1, BirthDate = DateTime.Now.AddYears(-1), HasOwner = false },
+            new Dog { Id = 6, Name = "Simba", Breed = "Boxer", Age = 6, BirthDate = DateTime.Now.AddYears(-6), HasOwner = true },
+            new Dog { Id = 7, Name = "Milo", Breed = "Beagle", Age = 2, BirthDate = DateTime.Now.AddYears(-2), HasOwner = false },
+            new Dog { Id = 8, Name = "Nina", Breed = "Shih Tzu", Age = 3, BirthDate = DateTime.Now.AddYears(-3), HasOwner = true },
+            new Dog { Id = 9, Name = "Bob", Breed = "Dachshund", Age = 7, BirthDate = DateTime.Now.AddYears(-7), HasOwner = true },
+            new Dog { Id = 10, Name = "Toby", Breed = "Rottweiler", Age = 5, BirthDate = DateTime.Now.AddYears(-5), HasOwner = false },
+            new Dog { Id = 11, Name = "Mel", Breed = "Yorkshire", Age = 4, BirthDate = DateTime.Now.AddYears(-4), HasOwner = true },
+            new Dog { Id = 12, Name = "Fred", Breed = "Schnauzer", Age = 3, BirthDate = DateTime.Now.AddYears(-3), HasOwner = false },
+            new Dog { Id = 13, Name = "Duke", Breed = "Husky Siberiano", Age = 2, BirthDate = DateTime.Now.AddYears(-2), HasOwner = true },
+            new Dog { Id = 14, Name = "Lola", Breed = "Pug", Age = 1, BirthDate = DateTime.Now.AddYears(-1), HasOwner = true },
+            new Dog { Id = 15, Name = "Spike", Breed = "Bulldog Francês", Age = 6, BirthDate = DateTime.Now.AddYears(-6), HasOwner = false },
+            new Dog { Id = 16, Name = "Maggie", Breed = "Cocker Spaniel", Age = 5, BirthDate = DateTime.Now.AddYears(-5), HasOwner = true },
+            new Dog { Id = 17, Name = "Bruce", Breed = "Doberman", Age = 4, BirthDate = DateTime.Now.AddYears(-4), HasOwner = true },
+            new Dog { Id = 18, Name = "Julie", Breed = "Border Collie", Age = 3, BirthDate = DateTime.Now.AddYears(-3), HasOwner = false },
+            new Dog { Id = 19, Name = "Pipoca", Breed = "Lhasa Apso", Age = 2, BirthDate = DateTime.Now.AddYears(-2), HasOwner = true },
+            new Dog { Id = 20, Name = "Thor", Breed = "Akita", Age = 1, BirthDate = DateTime.Now.AddYears(-1), HasOwner = false }
         };
 
         [HttpGet("connectionstring")]
@@ -26,7 +42,6 @@ namespace DogsWorld.Controllers
             return "Mocked connection string";
         }
 
-        // GET: api/Dogs
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Dog>>> GetDog()
         {
@@ -38,7 +53,6 @@ namespace DogsWorld.Controllers
         {
             var dog = _dogs.FirstOrDefault(d => d.Id == id);
 
-            // Só logue se houver escopo ativo do Datadog
             try
             {
                 DatadogLogger.AddTag("username", "eminsalim");
@@ -47,7 +61,6 @@ namespace DogsWorld.Controllers
             }
             catch (NullReferenceException)
             {
-                // Ignora se não houver escopo ativo
             }
 
             if (dog == null)
@@ -58,7 +71,6 @@ namespace DogsWorld.Controllers
             return await Task.FromResult(dog);
         }
 
-        // PUT: api/Dogs/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutDog(int id, Dog dog)
         {
@@ -82,7 +94,6 @@ namespace DogsWorld.Controllers
             return await Task.FromResult(NoContent());
         }
 
-        // POST: api/Dogs
         [HttpPost]
         public async Task<ActionResult<Dog>> PostDog(Dog dog)
         {
@@ -92,7 +103,6 @@ namespace DogsWorld.Controllers
             return await Task.FromResult(CreatedAtAction("GetDog", new { id = dog.Id }, dog));
         }
 
-        // DELETE: api/Dogs/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<Dog>> DeleteDog(int id)
         {
